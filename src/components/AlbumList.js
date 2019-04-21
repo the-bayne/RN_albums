063d1a3b621305becc4db429a01ef1b012658af6
Jared Thibodeaux <admin@baynecorp.com>
2019-04-20 23:06:59 -0500

    Inital Commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+	state = { albums: [] }; // Initial base empty state for Albums list
+
+	componentWillMount() { // When View loads
+		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+			.then(response => this.setState({ albums: response.data })); // Set response to albums array
+	}
+
+	renderAlbums() {
+     return (
+         <View>{ 
+             this.state.albums.map(album =>
+             <AlbumDetail key={album.title} album={album} />
+         )}</View>
+     );
+ }
+ 
+    render() {
+     return this.renderAlbums();
+    }
+}
+
+export default AlbumList;
